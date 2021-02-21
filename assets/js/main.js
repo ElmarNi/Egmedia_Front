fd1553983d4f5ffeccefac660847d07221bf9a71
unknown <elmarni@code.edu.az>
2021-02-21 13:15:44 +0400

    slider aside click event is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,22 @@ $(document).ready(function () {
         items: 1,
         dots: true
     })
+    $("#slider .slider-right-side-aside .right-side-item").each(function(index, element){
+        $(element).click(function () {
+            let slider_item = $(`#slider .slider-item[data-id=${$(element).attr("data-id")}]`)[0]
+            let width = $(slider_item).width()
+            let data_id = parseInt($(element).attr("data-id"))
+            // let index = 0;
+            // for (let i = 0; i < slider_item.length; i++) {
+            //     if (!$(slider_item[i]).parent().hasClass("cloned")) {
+            //         slider_item = $(slider_item[i])
+            //         console.log(i);
+            //     }
+            // }
+            $("#slider .owl-stage").css({
+                "transform": `translate3d(-${width * (data_id + 1)}px, 0px, 0px)`,
+                "transition": "all 0.25s ease 0s"
+            })
+        })
+    })
 });
