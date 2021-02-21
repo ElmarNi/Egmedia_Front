3a0c0042dd9d138418948e28352fda66362962ef
unknown <elmarni@code.edu.az>
2021-02-21 12:25:21 +0400

    slider without click

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,11 @@ $(document).ready(function () {
         $("header nav.menu").toggleClass("active")
         $("header").toggleClass("active")
     })
+    $('#slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        items: 1,
+        dots: true
+    })
 });
