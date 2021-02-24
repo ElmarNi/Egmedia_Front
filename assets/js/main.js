db8fcd637fcb19c354c274fa3cbec78022779d4a
unknown <elmarni@code.edu.az>
2021-02-24 17:10:45 +0400

    footer li in contact infos border right changed to border bottom in mobile, nav.menu size of li elements decreased

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $("header .menu-bar").click(function () {
         $("header nav.menu").toggleClass("active")
         $("header").toggleClass("active")
+        $("body").toggleClass("active")
     })
     $('#slider .owl-carousel').owlCarousel({
         loop:true,
